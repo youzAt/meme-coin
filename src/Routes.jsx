ebb671986ecc125b60fe0a67e6371c73b5bb97eb
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2025-03-04 03:36:12 +0330

    ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,17 +4,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainLayout from "./pages/MainLayout";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import ProtectedLayout from "./pages/ProtectedLayout";
 import Profile from "./pages/Profile";
-=======
 import Wallet from "./pages/Wallet";
->>>>>>> 174a1c218b151979bda79b7a3cde6f8a33915fba
 
 const publicRoutes = [];
 
 const authOnlyRoutes = [
-<<<<<<< HEAD
 	{
 		element: <ProtectedLayout />,
 		children: [
@@ -29,6 +25,10 @@ const authOnlyRoutes = [
 					{
 						path: "/profile",
 						element: <Profile />,
+					},
+					{
+						path: "/wallet",
+						element: <Wallet />,
 					},
 				],
 			},
@@ -50,47 +50,15 @@ const notAuthOnlyRoutes = [
 			},
 		],
 	},
-=======
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-            },
-            {
-                path: "/wallet",
-                element: <Wallet />,
-            },
-        ],
-    },
-];
-
-const notAuthOnlyRoutes = [
-    {
-        element: <LoginLayout />,
-        children: [
-            {
-                path: "/login",
-                element: <Login />,
-            },
-            {
-                path: "/signup",
-                element: <Signup />,
-            },
-        ],
-    },
->>>>>>> 174a1c218b151979bda79b7a3cde6f8a33915fba
 ];
 
 const router = createBrowserRouter([
-    ...publicRoutes,
-    ...authOnlyRoutes,
-    ...notAuthOnlyRoutes,
+	...publicRoutes,
+	...authOnlyRoutes,
+	...notAuthOnlyRoutes,
 ]);
 const Routes = () => {
-    return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 };
 
 export default Routes;
